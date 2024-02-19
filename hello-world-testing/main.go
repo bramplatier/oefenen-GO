a90f43400e871d92f00cef84ffb04ddcdab92d7d
bramplatier <145543039+bramplatier@users.noreply.github.com>
2024-02-19 13:50:17 +0100

    Update main.go

--- hello-world-testing/main.go
+++ hello-world-testing/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var groet = bepaalgroet()
+	fmt.Printf("%v! Welkom bij Fonteyn Vakantieparken\n", groet)
+}
+func bepaalgroet() string {
+	hour := time.Now().Hour()
+	var groet string
+	if hour >= 7 && hour < 12 {
+		groet = "goedemorgen"
+	} else if hour >= 12 && hour < 18 {
+		groet = "goedemiddag"
+	} else if hour >= 18 && hour < 23 {
+		groet = "goedenavond"
+	} else {
+		groet = "Sorry, de parkeerplaats is s nachts gesloten"
+	}
+	return groet
 }
